486e645c97555760d624e747653d813b33b99986
Prashanna_31 <prashanna.r@idp.com>
2025-04-10 09:31:52 +0530

    update port

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def download_excel():
                      as_attachment=True,
                      mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
